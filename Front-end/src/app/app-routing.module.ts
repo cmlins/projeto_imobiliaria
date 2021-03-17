fef7dcd2c3cf1805b39eb83afda4771a63cd5bd4
Cinthya Lins <cml2@cin.ufpe.br>
2021-03-17 00:23:00 -0300

    cadastrar dados do comprador na base ok

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListarImoveisComponent } from './listar-imoveis/listar-imoveis.component';
 import { NovoImovelComponent } from './novo-imovel/novo-imovel.component';
+import { ComprarImovelComponent } from './comprar-imovel/comprar-imovel.component';
 
 const routes: Routes = [
   {path:'listar-imoveis', component: ListarImoveisComponent},
   {path:'novo-imovel', component: NovoImovelComponent},
+  {path:'comprar-imovel', component: ComprarImovelComponent},
 ];
 
 @NgModule({
--- Front-end/src/app/comprar-imovel/comprar-imovel.component.ts
+++ Front-end/src/app/comprar-imovel/comprar-imovel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api-service.service';
 
 @Component({
   selector: 'app-comprar-imovel',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComprarImovelComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  insereNovoCliente
+    (
+      nome: string,
+      cpf: string,
+      data_nasc: string,
+      rg: string,
+      profissao: string ,
+      estado_civil: string,
+      rua_prop: string,
+      numero_prop: string,
+      andar_prop: string,
+      bloco_prop: string,
+      bairro_prop: string,
+      cep_prop: string,
+      cidade_prop: string,
+      uf_prop: string,
+    )
+    {
+    this.apiService.postCliente({
+        "pessoa":
+        {
+          "nome": nome,
+          "cpf": cpf,
+          "data_nasc": data_nasc,
+          "rg": rg,
+          "profissao": profissao,
+          "estado_civil": estado_civil,
+        },
+        "endereco":
+        {
+          "rua": rua_prop,
+          "numero": numero_prop,
+          "andar": andar_prop,
+          "bloco": bloco_prop,
+          "bairro": bairro_prop,
+          "cep": cep_prop,
+          "cidade": cidade_prop,
+          "uf": uf_prop,
+        },
+      }).subscribe(data => {
+      console.log(data)
+    },
+    error  => {
+      console.log("Error", error);
+      console.log(data_nasc)
+    });
   }
-
 }
